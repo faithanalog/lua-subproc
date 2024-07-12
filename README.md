63cd56a84b1677422041ecc059fb5c09111e901e
Artemis Everfree <artemis@artemis.sh>
2024-07-11 18:17:27 -0700

    Rework the library to allow for more control over runner behavior.
    Hopefully this helps me get around a lot of paper cuts I have whenever I decide
    I want error handling, logging, or IO redirection. The new runner creator also
    has what I feel like is probably a better default of not inheriting stdin, but
    for backwards compat I've left the top level functions using a runner that
    inherits stdin.
    
    I'll cut a release after I've tested this with my projects and confirmed that
    it indeed does not break stuff

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
+Wrapper around `io.popen()`/`os.execute()` designed to make these functions
+easier to use safely. Only useable on unix systems- won't do anything that
+makes sense on windows.
+
+These stdlib functions have some inherent limitations. If you find yourself
+running into those limits, you should go use
+[luaposix](https://luaposix.github.io/luaposix/modules/posix.html), which has a
+much more powerful set of tools for launching child processes, including the
+ability to create pipelines of processes.
+
 ## install
 
 ```
@@ -7,29 +17,180 @@ luarocks install subproc
 ## simple usage
 
 ```
-subproc = require 'subproc'
+local subproc = require 'subproc'
 
 local output, exit_reason, status = subproc('ls', '-l', '/')
-print(output)
+print(output, exit_reason, status)
 ```
 
 you can also use `io.popen(subproc.escape_and_join('command', 'arg1', 'arg2...'))` to safely implement anything that popen can do but this library can't.
 
 
-## exported functions
+## basic exported functions
 
 - `subproc.escape_and_join(...)`: tostring() and shell-escape all arguments,
   then join them with spaces. output is suitable to provide to `io.popen` or
   `os.execute`.
+  - alias: `subproc.esc()`
 - `subproc.shell(cmdline)`: passes `cmdline` to `io.popen` and captures output.
   returns `output, exit_reason, status` where `exit_reason, status` come from
   `io.close()`
 - `subproc.subproc(...)`: escapes all arguments with `subproc.escape_and_join`,
   and runs with `subproc.shell`.
 - `subproc(...)`: alias of `subproc.subproc`
+- `subproc.lines(...)`: escapes all arguments, returns an iterator over lines.
+  The iterator receives each line with the trailing newline still attached.
+- `subproc.last_exit()`: Returns the exit code of the last command that finished
+  running. Useful to get the exit code of a command after consuming all the
+  lines from `subproc.lines()`. Do be careful with this one though. If you find
+  yourself launching multiple processes in parallel, and you need their exit
+  statuses, go use `popen` manually instead of using this function.
 
 
-## planned features
+## custom runners
 
-i'd like to add things in future to allow providing stdin, or interactive use
-with coroutines.
+All the top level functions in `subproc` are actually functions of the default
+subproc runner. You can create custom runners that do different things with
+stdin/stdout/stderr, or do automatic error handling,
+
+- `subproc.runner()`: Create a custom runner. Docs for this are extensive, so
+  see them below this function list.
+- `runner.extend(conf)`: Create a custom runner, inheriting the config of the
+  source runner and overriding it with new values in `conf`
+- `runner.run_with(conf)`: Creates a new runner with `runner.extend`, and then
+  runs the command specified by the table `conf.cmd`
+- `runner.lines_with(conf)`: Creates a new runner with `runner.extend`, and then
+  runs the command specified by the table `conf.cmd`. Returns an iterator over
+  the lines
+
+
+### `subproc.runner()`
+
+Returns a `runner` which lets you have a bit more flexibility over how
+running works.
+
+    local runner = subproc.runner {
+        log = nil | true | string | function,
+        err = nil | true | function,
+
+        stdin  = nil | PIPE_MODE | '/path/to/file'
+        stdout = nil | PIPE_MODE | '/path/to/file',
+        stderr = nil | PIPE_MODE | '/path/to/file'
+    }
+
+    local output, exit_reason, exit_code = runner('some', 'command')
+
+    for ln in runner.lines('some', 'command') do
+        -- `ln` includes the trailing '\n'
+    end
+    local exit_reason, exit_code = run.last_exit()
+
+#### `log`
+
+when `log` is non-nil, all command output will be read from the child
+process line by line, even if it's going to be returned as a single string
+to your code.
+
+When `log` is true, command output will be repeated to stdout with `print()`
+
+When `log` is a string, command output will be repeated to stdout as with
+`true`, but the string will be prepended as a prefix to each line
+
+When `log` is a function, that function will be called with each lines of
+input. This shares some functionality overlap with .lines(), but it's
+intended to make it easy to pass in a custom logger function. The function
+will be called with 2 arguments,
+
+    log(line_of_output, cmd)
+
+where `cmd` will the command that was passed to io.popen()
+
+#### `err`
+
+When `err` is true, the runner will call the lua stdlib error() function
+if the process exits from a signal, or with a none-zero exit code.
+
+When `err` is a function, the runner will call that function under the same
+conditions:
+    err(exit_reason, exit_code, cmd)
+
+#### `stdin`, `stdout`, and `stderr`
+
+io.popen is pretty limited in what it can do with stdin/stdout/stderr. We
+can only capture the stdout stream of the procces, not stderr. we can choose
+to write to the process's stdin, but if we do that we cannot read its
+stdout.
+
+But, we are just running a shell command after all, so we can use shell
+redirects to work within these limitations.
+
+These arguments all append shell IO redirections to the command passed in.
+If you want to handle these redirections entirely yourself, here is what you
+should do:
+
+- Set `stdin` to `subproc.PIPE_INHERIT`
+- Set `stdout` to `subproc.PIPE_CAPTURE`, or leave it as nil
+- Set `stderr` to `subproc.PIPE_INHERIT`, or leave it as nil
+
+This will ensure no redirections are added.
+
+#### `stdin`
+
+By default, `stdin` defaults to `subproc.PIPE_DEVNULL`:
+
+    command < /dev/null
+
+This is the default because giving your main process's stdin to your
+subcommand is probably not what you intended. If you did intend that, you
+can set `stdin` to `subproc.PIPE_INHERIT`, and the command will use your
+process's stdin
+
+    command
+
+You can also set it to a filepath to direct stdin from the file at that path.
+
+    command < /some/file
+
+#### `stdout`
+
+Usually when running commands, we want stdout. So, when stdout is `nil`,
+we default to `subproc.PIPE_CAPTURE`.
+
+If `stdout` is `subproc.PIPE_CAPTURE`, the command's stdout will be captured
+and returned in the string output of the command. Note that if you also have
+`stderr` set to PIPE_CAPTURE, your command output will have both stdout and
+stderr mixed together into a single output stream.
+
+If `stdout` is `subproc.PIPE_DEVNULL`, it will be redirected to /dev/null
+
+If `stdout` is a filepath, stdout will be redirected to that file.
+
+`subproc.PIPE_INHERIT` can be confusing here, so be careful. If `stdout` is
+`subproc.PIPE_INHERIT`, we will execute the command with `os.execute()`
+instead of `io.popen()`. This has two major implications:
+
+1. There is no way to actually capture stderr when stdout is PIPE_INHERIT
+2. If stderr is set to PIPE_CAPTURE, it will be redirected to the the
+   inherited stdout.
+
+As such, the `log` function will also never run, even if stderr is set to
+capture, because capturing is impossible in this scenario.
+
+
+#### `stderr`
+
+`stderr` defaults to `subproc.PIPE_INHERIT`
+
+If `stderr` is `subproc.PIPE_CAPTURE`, the process's stderr will captured
+and returned in the string output of the command. Note that if you also have
+`stdout` set to PIPE_CAPTURE, your command output will have both stdout and
+stderr mixed together into a single output stream.
+
+If `stderr` is `subproc.PIPE_DEVNULL`, it will be redirected to /dev/null
+
+If `stderr` is a filepath, stderr will be redirected to that file.
+
+If `stderr` is `subproc.PIPE_INHERIT`, stderr is not redirected at all, and
+will this inherit the parent process's stderr. There are no strange caveats
+with this the way there is for stdout's PIPE_INHERIT behavior.
+
--- subproc.lua
+++ subproc.lua
@@ -1,9 +1,17 @@
 local M = {}
 
+-- IO redirection types
+local PIPE_INHERIT = 1
+local PIPE_CAPTURE = 2
+local PIPE_DEVNULL = 3
+
+M.PIPE_INHERIT = PIPE_INHERIT
+M.PIPE_CAPTURE = PIPE_CAPTURE
+M.PIPE_DEVNULL = PIPE_DEVNULL
+
 -- escape shell argumengs
-M.escape_and_join = function(...)
-    local args = {...}
-    local cmdline = ''
+local function escape_and_join_tbl(args)
+    local cmdline = {}
 
     for _, arg in pairs(args) do
         arg = tostring(arg)
@@ -16,7 +24,7 @@ M.escape_and_join = function(...)
         --
         -- this is an escaped quote in sh: '"'"'
         --
-        -- the way this works is, 
+        -- the way this works is,
         --   - the first single-quote closes the current string
         --   - then we have a double-quote string containing a single-quote
         --   - the final single-quote opens a new string
@@ -25,24 +33,462 @@ M.escape_and_join = function(...)
         --
         -- this gets a little more absurd because we need to write the escaped
         -- quote inside a lua string. so the lua string is "'\"'\"'"
-        arg = "'" .. arg:gsub("'", "'\"'\"'") .. "'"
 
-        cmdline = cmdline .. ' ' .. arg
+        -- Add space to separate args, unless this is the first arg
+        if #cmdline > 0 then
+            table.insert(cmdline, ' ')
+        end
+
+        table.insert(cmdline, "'")
+        table.insert(cmdline, (arg:gsub("'", "'\"'\"'")))
+        table.insert(cmdline, "'")
     end
 
     -- remove leading space
-    return cmdline:sub(2)
+    return table.concat(cmdline)
 end
 
-M.shell = function(cmd)
-    local p = io.popen(cmd, 'r')
-    local output = p:read('*a')
-    local _, exit_reason, status = p:close()
-    return output, exit_reason, status
+function M.escape_and_join(...)
+    return escape_and_join_tbl({...})
 end
 
-M.subproc = function(...)
-    return M.shell(M.escape_and_join(...))
+-- convenience alias
+M.esc = M.escape_and_join
+
+--[[
+Returns a `runner` which lets you have a bit more flexibility over how
+running works.
+
+    local runner = subproc.runner {
+        log = nil | true | string | function,
+        err = nil | true | function,
+
+        stdin  = nil | PIPE_MODE | '/path/to/file'
+        stdout = nil | PIPE_MODE | '/path/to/file',
+        stderr = nil | PIPE_MODE | '/path/to/file'
+    }
+
+    local output, exit_reason, exit_code = runner('some', 'command')
+
+    for ln in runner.lines('some', 'command') do
+        -- `ln` includes the trailing '\n'
+    end
+    local exit_reason, exit_code = run.last_exit()
+
+#### `log`
+
+when `log` is non-nil, all command output will be read from the child
+process line by line, even if it's going to be returned as a single string
+to your code.
+
+When `log` is true, command output will be repeated to stdout with `print()`
+
+When `log` is a string, command output will be repeated to stdout as with
+`true`, but the string will be prepended as a prefix to each line
+
+When `log` is a function, that function will be called with each lines of
+input. This shares some functionality overlap with .lines(), but it's
+intended to make it easy to pass in a custom logger function. The function
+will be called with 2 arguments,
+
+    log(line_of_output, cmd)
+
+where `cmd` will the command that was passed to io.popen()
+
+#### `err`
+
+When `err` is true, the runner will call the lua stdlib error() function
+if the process exits from a signal, or with a none-zero exit code.
+
+When `err` is a function, the runner will call that function under the same
+conditions:
+    err(exit_reason, exit_code, cmd)
+
+#### `stdin`, `stdout`, and `stderr`
+
+io.popen is pretty limited in what it can do with stdin/stdout/stderr. We
+can only capture the stdout stream of the procces, not stderr. we can choose
+to write to the process's stdin, but if we do that we cannot read its
+stdout.
+
+But, we are just running a shell command after all, so we can use shell
+redirects to work within these limitations.
+
+These arguments all append shell IO redirections to the command passed in.
+If you want to handle these redirections entirely yourself, here is what you
+should do:
+
+- Set `stdin` to `subproc.PIPE_INHERIT`
+- Set `stdout` to `subproc.PIPE_CAPTURE`, or leave it as nil
+- Set `stderr` to `subproc.PIPE_INHERIT`, or leave it as nil
+
+This will ensure no redirections are added.
+
+#### `stdin`
+
+By default, `stdin` defaults to `subproc.PIPE_DEVNULL`:
+
+    command < /dev/null
+
+This is the default because giving your main process's stdin to your
+subcommand is probably not what you intended. If you did intend that, you
+can set `stdin` to `subproc.PIPE_INHERIT`, and the command will use your
+process's stdin
+
+    command
+
+You can also set it to a filepath to direct stdin from the file at that path.
+
+    command < /some/file
+
+#### `stdout`
+
+Usually when running commands, we want stdout. So, when stdout is `nil`,
+we default to `subproc.PIPE_CAPTURE`.
+
+If `stdout` is `subproc.PIPE_CAPTURE`, the command's stdout will be captured
+and returned in the string output of the command. Note that if you also have
+`stderr` set to PIPE_CAPTURE, your command output will have both stdout and
+stderr mixed together into a single output stream.
+
+If `stdout` is `subproc.PIPE_DEVNULL`, it will be redirected to /dev/null
+
+If `stdout` is a filepath, stdout will be redirected to that file.
+
+`subproc.PIPE_INHERIT` can be confusing here, so be careful. If `stdout` is
+`subproc.PIPE_INHERIT`, we will execute the command with `os.execute()`
+instead of `io.popen()`. This has two major implications:
+
+1. There is no way to actually capture stderr when stdout is PIPE_INHERIT
+2. If stderr is set to PIPE_CAPTURE, it will be redirected to the the
+   inherited stdout.
+
+As such, the `log` function will also never run, even if stderr is set to
+capture, because capturing is impossible in this scenario.
+
+
+#### `stderr`
+
+`stderr` defaults to `subproc.PIPE_INHERIT`
+
+If `stderr` is `subproc.PIPE_CAPTURE`, the process's stderr will captured
+and returned in the string output of the command. Note that if you also have
+`stdout` set to PIPE_CAPTURE, your command output will have both stdout and
+stderr mixed together into a single output stream.
+
+If `stderr` is `subproc.PIPE_DEVNULL`, it will be redirected to /dev/null
+
+If `stderr` is a filepath, stderr will be redirected to that file.
+
+If `stderr` is `subproc.PIPE_INHERIT`, stderr is not redirected at all, and
+will this inherit the parent process's stderr. There are no strange caveats
+with this the way there is for stdout's PIPE_INHERIT behavior.
+
+]]
+M.runner = function(args)
+    local log = args.log
+    local err = args.err
+    local stdin = args.stdin
+    local stdout = args.stdout
+    local stderr = args.stderr
+
+    -- TODO document this
+    local drop_output = args.drop_output
+
+    -- Construct the IO redirects as needed for stdin/stdout/stderr
+    -- Start off with a spacer string to space our redirects away from the cmd
+    local redirects = {' '}
+    local inherit_stdout = false
+
+    -- stdin
+    if stdin == nil or stdin == PIPE_DEVNULL then
+        table.insert(redirects, '</dev/null')
+    elseif stdin == PIPE_INHERIT then
+        -- Append nothing
+    elseif stdin == PIPE_CAPTURE then
+        error('cannot PIPE_CAPTURE stdin')
+    elseif type(stdin) == 'string' then
+        table.insert(redirects, '<' .. M.escape_and_join(stdin))
+    end
+
+
+    --[[
+    stderr
+    it's very important that this happen before stdout, because shell redirects
+    are stateful. By redirecting stderr first, we can set it to the default
+    value of stdout, and then change stdout.
+    ]]
+    if stderr == nil or stderr == PIPE_INHERIT then
+        -- Append nothing
+    elseif stderr == PIPE_CAPTURE then
+        table.insert(redirects, '2>&1')
+    elseif stderr == PIPE_DEVNULL then
+        table.insert(redirects, '2>/dev/null')
+    elseif type(stderr) == 'string' then
+        table.insert(redirects, '2>'.. M.escape_and_join(stderr))
+    end
+
+    -- stdout
+    if stdout == nil or stdout == PIPE_CAPTURE then
+        -- Append nothing
+    elseif stdout == PIPE_INHERIT then
+        -- Append nothing BUT we need to switch to os.execute!!!
+        inherit_stdout = true
+    elseif stdout == PIPE_DEVNULL then
+        table.insert(redirects, '>/dev/null')
+    elseif type(stdout) == 'string' then
+        table.insert(redirects, '>' .. M.escape_and_join(stdout))
+    end
+
+    local redirect_suffix = table.concat(redirects, ' ')
+
+    -- Generate our error handler. It'll be a noop if `err` is nil.
+    local err_type = type(err)
+    if err == nil then
+        -- no-op
+        err = function()
+        end
+
+    elseif err_type == 'function' then
+        -- nothing to do
+
+    elseif err_type == 'boolean' and err then
+        -- default error handler
+        err = function(exit_reason, exit_code, cmd)
+            error(cmd .. ': cmd died by ' .. exit_reason .. ' with code ' .. exit_code)
+        end
+
+    else
+        error('invalid value type for `err`: ' .. err_type)
+
+    end
+
+    -- Generate our log handler
+    local log_type = type(log)
+    if log == nil then
+        -- do nothing!
+
+    elseif log_type == 'string' then
+        local prefix = log
+        log = function(str)
+            io.write(prefix)
+            io.write(str)
+            io.flush()
+        end
+
+    elseif log_type == 'boolean' and log then
+        log = function(str)
+            io.write(str)
+            io.flush()
+        end
+
+    elseif log_type == 'function' then
+        -- leave the logger as it is
+
+    else
+        error('invalid value type for `log`: ' .. log_type)
+
+    end
+
+    -- Define these up in this scope so that we can return them with last_exit()
+    local success, exit_reason, exit_code
+
+    local runner = {}
+
+    function runner.last_exit()
+        return exit_reason, exit_code
+    end
+
+    --[[
+    We'll change what shell() and lines() does depending on if we're inheriting
+    stdout and if we're logging.
+
+    Note that all of these set the success/exit_reason/exit_code variables
+    defined above, that way a program can query them with last_exit()
+    ]]
+    if inherit_stdout then
+        -- Simple os.execute() runner
+        function runner.shell(cmd)
+            success, exit_reason, exit_code = os.execute(cmd .. redirect_suffix)
+            if not success then
+                err(exit_reason, exit_code, cmd .. redirect_suffix)
+            end
+        end
+
+        runner.shell_lines = function(cmd)
+            runner.shell(cmd)
+
+            -- We won't have any lines to return, but still return a valid
+            -- iterator
+            return coroutine.wrap(function()
+            end)
+        end
+
+    elseif log then
+        -- Collect output while logging it line by line. We can define shell()
+        -- in terms of lines() for this one.
+        function runner.shell(cmd)
+            local output = {}
+            for line in runner.shell_lines(cmd) do
+                table.insert(output, line)
+            end
+            return table.concat(output), runner.last_exit()
+        end
+
+        function runner.shell_lines(cmd)
+            local p = io.popen(cmd .. redirect_suffix, 'r')
+
+            return coroutine.wrap(function()
+                local ln = p:read('L')
+                while ln do
+                    log(ln)
+                    coroutine.yield(ln)
+                    ln = p:read('L')
+                end
+
+                success, exit_reason, exit_code = p:close()
+                if not success then
+                    err(exit_reason, exit_code, cmd .. redirect_suffix)
+                end
+            end)
+        end
+
+    else
+        -- Read all input in one go. Ideal for reading lots of data into a
+        -- buffer
+        function runner.shell(cmd)
+            local p = io.popen(cmd .. redirect_suffix, 'r')
+            local output = p:read('a')
+            success, exit_reason, exit_code = p:close()
+            if not success then
+                err(exit_reason, exit_code, cmd .. redirect_suffix)
+            end
+            return output, exit_reason, exit_code
+        end
+
+        -- works like the lines() when logging, but without the logging
+        function runner.shell_lines(cmd)
+            local p = io.popen(cmd .. redirect_suffix, 'r')
+
+            return coroutine.wrap(function()
+                local ln = p:read('L')
+                while ln do
+                    coroutine.yield(ln)
+                    ln = p:read('L')
+                end
+
+                success, exit_reason, exit_code = p:close()
+                if not success then
+                    err(exit_reason, exit_code, cmd .. redirect_suffix)
+                end
+            end)
+        end
+    end
+
+    if drop_output then
+        -- Useful if you want to log stuff but you don't want to actually do
+        -- anything with the output. No reason to buffer it in that case.
+        runner.subproc = function(...)
+            for ln in runner.shell_lines(M.escape_and_join(...)) do
+            end
+            return nil, runner.last_exit()
+        end
+
+        runner.lines = function(...)
+            for ln in runner.shell_lines(M.escape_and_join(...)) do
+            end
+            return coroutine.wrap(function()
+            end)
+        end
+    else
+        -- normal behavior
+        runner.subproc = function(...)
+            return runner.shell(M.escape_and_join(...))
+        end
+
+        runner.lines = function(...)
+            return runner.shell_lines(M.escape_and_join(...))
+        end
+    end
+
+    --[[
+    convenience, so we can do like
+
+    require('subproc').runner { }
+
+    without having to keep the original subproc ref around for escape
+    ]]
+    runner.escape_and_join = M.escape_and_join
+    runner.esc = M.escape_and_join
+    runner.PIPE_INHERIT = PIPE_INHERIT
+    runner.PIPE_CAPTURE = PIPE_CAPTURE
+    runner.PIPE_DEVNULL = PIPE_DEVNULL
+
+
+    --[[
+    A few shortcuts for instantiating a one-off runner with config slightly
+    modified, and then running a command with it. Specify configuration as
+    with runner(), but also specify a command as
+        cmd = { 'some', 'args' }
+    ]]
+    function runner.extend(extra_args)
+        local sub_args = {}
+        for k, v in pairs(args) do
+            sub_args[k] = v
+        end
+        for k, v in pairs(extra_args) do
+            sub_args[k] = v
+        end
+        return M.runner(sub_args)
+    end
+
+    function runner.run_with(extra_args)
+        assert(extra_args.cmd)
+        local r = runner.extend(extra_args)
+        return r.shell(escape_and_join_tbl(extra_args.cmd))
+    end
+
+    function runner.lines_with(extra_args)
+        assert(extra_args.cmd)
+        local r = runner.extend(extra_args)
+        return r.shell_lines(escape_and_join_tbl(extra_args.cmd))
+    end
+
+    setmetatable(runner, {__call = function (_, ...) return runner.subproc(...) end })
+
+    return runner
+end
+
+
+-- Create a default runner, which maintains the original subproc behavior
+local default_runner = M.runner {
+    log = nil,
+    err = nil,
+    stdin = PIPE_INHERIT,
+    stdout = PIPE_CAPTURE,
+    stderr = PIPE_INHERIT
+}
+
+-- Define the toplevel functions from this runner
+for k, v in pairs(default_runner) do
+    if type(v) == 'function' then
+        M[k] = v
+    end
+end
+
+-- A few shortcuts for instantiating a one-off runner and then running a command
+-- with it. Specify configuration as with runner, but also specify a command as
+-- cmd = { 'some', 'args' }
+function M.run_with(args)
+    assert(args.cmd)
+    local runner = M.runner(args)
+    return runner.shell(escape_and_join_tbl(args.cmd))
+end
+
+function M.lines_with(args)
+    assert(args.cmd)
+    local runner = M.runner(args)
+    return runner.shell_lines(escape_and_join_tbl(args.cmd))
 end
 
 -- allow subproc() as shorthand for subproc.subproc
